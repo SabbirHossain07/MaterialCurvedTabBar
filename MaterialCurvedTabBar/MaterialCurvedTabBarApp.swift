281ba599aa5a75ca8ba22a17477d6b551500ecd9
SabbirHossain07 <sabbirsopnil07@gmail.com>
2022-06-12 19:45:00 +0600

    Initial Commit

---
+++ MaterialCurvedTabBar/MaterialCurvedTabBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  MaterialCurvedTabBarApp.swift
+//  MaterialCurvedTabBar
+//
+//  Created by Sopnil Sohan on 12/6/22.
+//
+
+import SwiftUI
+
+@main
+struct MaterialCurvedTabBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
